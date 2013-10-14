ce749a9bf944da60c3e2c2d44c0ffae0f5e38690
Sören Gade <gade.soeren@googlemail.com>
2013-10-14 22:31:42 +0200

    Added DetailViewController.

---
+++ Feedo-iOS/DetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.h
+//  Feedo-iOS
+//
+//  Created by Sören Gade on 14.10.13.
+//  Copyright (c) 2013 Sören Gade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
